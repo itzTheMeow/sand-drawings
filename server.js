f621a00e0938b29b712e57491f71c0a221969c0b
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-07-15 10:52:15 -0400

    css parser

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const sass = require("sass");
 
 const config = require("./config.json");
 const app = express();
+
+let priorityStyle = "";
+let allStyle = "";
+fs.readdirSync(__dirname + "/../client/css").forEach((css) => {
+  let compiled = sass.renderSync({ file: `${__dirname}/../client/css/${css}` });
+  if (css.startsWith("_")) priorityStyle += String(`${compiled.css}\n`);
+  else allStyle += String(`${compiled.css}\n`);
+});
+priorityStyle = priorityStyle.replace(/;\n/g, " !important;\n");
+if (allStyle) fs.writeFileSync("client/compiled.css", `${allStyle}\n${priorityStyle}`);
+else console.error("No css.");
 
 app.use(express.static(__dirname + "/site"));
 
