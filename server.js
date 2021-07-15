04b039e7e2265f2ac4d5c763e4fe8494451e681f
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-07-15 02:28:21 -0400

    add webpack

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require("express");
 const config = require("./config.json");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("ok");
-});
+app.use(express.static(__dirname + "/site"));
 
 app.listen(config.port, () => {
   console.log("Sand Drawings is online and listening on port " + config.port);
