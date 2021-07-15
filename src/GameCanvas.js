408081eab7e5273b0773db516050a738c0d0d6dd
itzTheMeow <ez@clap.com>
2021-07-15 14:09:42 -0400

    add sizing

--- src/GameCanvas.js
+++ src/GameCanvas.js
@@ -1,5 +1,19 @@
 import _ from "./util/_"
 
 let GameCanvas = _("canvas");
+let ctx = GameCanvas.getContext("2d");
 
-export default GameCanvas;
+let canvasSize = {
+  w: 0,
+  h: 0
+}
+function getSize() {
+  return canvasSize;
+}
+function updateSizing() {
+  canvasSize.w = GameCanvas.width = window.innerWidth;
+  canvasSize.h = GameCanvas.height = window.innerHeight;
+}
+updateSizing();
+
+export { GameCanvas, ctx, getSize };
--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import config from "./config";
-import GameCanvas from "./GameCanvas";
+import { GameCanvas, ctx, getSize } from "./GameCanvas";
 
-let ctx = GameCanvas.getContext("2d");
-ctx.fillColor = config.background;
-ctx.fillRect(0, 0, 10, 10)
+let size = getSize();
+ctx.fillStyle = config.background;
+ctx.fillRect(0, 0, size.w, size.h);
