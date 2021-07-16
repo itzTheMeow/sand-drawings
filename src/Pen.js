f102b5197ab07112705107ef34fe9d4d24f8ed56
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-07-16 01:28:30 -0400

    add mousePos

--- src/Pen.js
+++ src/Pen.js
@@ -2,6 +2,7 @@ function Pen(game) {
   this.size = 5;
 
   this.isDrawing = false;
+  this.mousePos = { x: 0, y: 0 };
   this.startDrawing = function () {
     this.isDrawing = true;
   };
@@ -12,9 +13,15 @@ function Pen(game) {
   game.canvas.onmouseup = this.stopDrawing;
   game.canvas.ontouchstart = this.startDrawing;
   game.canvas.ontouchend = this.stopDrawing;
+  game.canvas.onmousemove = function (e) {
+    this.mousePos = {
+      x: e.offsetX,
+      y: e.offsetY,
+    };
+  };
 
   this.update = function () {
-    if (this.isDrawing) game.pixels["10,10"] = 1;
+    if (this.isDrawing) game.pixels[`${this.mousePos.x},${this.mousePos.y}`] = 1;
   };
 }
 
