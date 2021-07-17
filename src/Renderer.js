359e65b6d4366646d47ec12db7a263fbef2b2946
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-07-16 23:55:17 -0400

    attempt to fix

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,33 +1,30 @@
 import { getMaterial } from "./Materials";
 import config from "./config";
-import { hexToRgb } from "./util/HEX2RGB"
+import { hexToRgb } from "./util/HEX2RGB";
 
 function Renderer(game) {
-  this.imgData = game.ctx.createImageData(game.canvas.width,game.canvas.height);
+  this.imgData = game.ctx.createImageData(game.canvas.width, game.canvas.height);
   this.pixData = this.imgData.data;
-  
+
   this.renderPixel = function (x, y, type) {
     let color = hexToRgb(getMaterial(type).color);
     let i = x * 4 + y * (game.canvas.height * 4);
 
-  this.pixData[i] = color[0]; // red
-  this.pixData[i + 1] = color[1]; // green
-  this.pixData[i + 2] = color[2]; // blue
-  this.pixData[i + 3] = 255; // blue
+    this.pixData[i] = color[0]; // red
+    this.pixData[i + 1] = color[1]; // green
+    this.pixData[i + 2] = color[2]; // blue
+    this.pixData[i + 3] = 255; // blue
   };
-  this.clear = function() {
+  this.clear = function () {
     for (let i = 0; i < this.imgData.data.length; i++) {
-     this.imgData.data[i + 3] = 0;
+      this.imgData.data[i + 3] = 0;
     }
   };
-  this.finishFrame = function() {
+  this.finishFrame = function () {
     game.ctx.putImageData(this.imgData, 0, 0);
-  }
+  };
 
   this.update = function () {
-    game.ctx.fillStyle = config.background;
-    //game.ctx.fillRect(0, 0, game.canvas.width, game.canvas.height);
-
     Object.keys(game.pixels).forEach((p) => {
       let pixel = {
         x: Number(p.split(",")[0]),
@@ -36,6 +33,7 @@ function Renderer(game) {
       };
       this.renderPixel(pixel.x, pixel.y, pixel.mat);
     });
+    this.finishFrame();
     requestAnimationFrame(this.update.bind(this));
   };
   this.startRender = function () {
