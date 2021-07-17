3f206f0db2f2f7b6f80558dab0d52e378ae72119
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-07-17 00:08:08 -0400

    New render (#2)
    * not copy and paste in the engine
    
    * attempt to fix
    
    Co-authored-by: itzTheMeow <ez@clap.com>

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,16 +1,30 @@
 import { getMaterial } from "./Materials";
 import config from "./config";
+import { hexToRgb } from "./util/HEX2RGB";
 
 function Renderer(game) {
+  this.imgData = game.ctx.createImageData(game.canvas.width, game.canvas.height);
+  this.pixData = this.imgData.data;
+
   this.renderPixel = function (x, y, type) {
-    game.ctx.fillStyle = getMaterial(type).color;
-    game.ctx.fillRect(x, y, 1, 1);
+    let color = hexToRgb(getMaterial(type).color);
+    let i = x * 4 + y * (game.canvas.height * 4);
+
+    this.pixData[i] = color[0]; // red
+    this.pixData[i + 1] = color[1]; // green
+    this.pixData[i + 2] = color[2]; // blue
+    this.pixData[i + 3] = 255; // blue
+  };
+  this.clear = function () {
+    for (let i = 0; i < this.imgData.data.length; i++) {
+      this.imgData.data[i + 3] = 0;
+    }
+  };
+  this.finishFrame = function () {
+    game.ctx.putImageData(this.imgData, 0, 0);
   };
 
   this.update = function () {
-    game.ctx.fillStyle = config.background;
-    //game.ctx.fillRect(0, 0, game.canvas.width, game.canvas.height);
-
     Object.keys(game.pixels).forEach((p) => {
       let pixel = {
         x: Number(p.split(",")[0]),
@@ -19,6 +33,7 @@ function Renderer(game) {
       };
       this.renderPixel(pixel.x, pixel.y, pixel.mat);
     });
+    this.finishFrame();
     requestAnimationFrame(this.update.bind(this));
   };
   this.startRender = function () {
